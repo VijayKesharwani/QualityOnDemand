f4d8f959d41866bccd6173bd36f735f1e8e2427e
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-10-19 15:46:31 +0530

    Create check-telco-specific-terms.js

---
+++ lint_function/check-telco-specific-terms.js
@@ -0,0 +1,31 @@
+const { createSuggestion } = require("@stoplight/spectral-functions");
+
+module.exports = {
+  suggestInclusiveTerms: (target, inclusiveTerms, replacements) => {
+    const messages = [];
+
+    replacements.forEach((replacement) => {
+      const original = replacement.original;
+      const recommended = replacement.recommended;
+
+      const regex = new RegExp(replaceAll(original, /[.*+?^${}()|[\]\\]/g, "\\$&"), "g");
+
+      target.replace(regex, (match, offset) => {
+        messages.push(
+          createSuggestion(`Consider replacing '${original}' with '${recommended}'.`, {
+            range: {
+              start: offset,
+              end: offset + original.length,
+            },
+          })
+        );
+      });
+    });
+
+    return messages;
+  },
+};
+
+function replaceAll(string, search, replace) {
+  return string.split(search).join(replace);
+}
