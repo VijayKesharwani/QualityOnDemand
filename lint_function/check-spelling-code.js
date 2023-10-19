9829f9bb4e2cbd458a58e9c1ea2328e99afc2d39
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-10-19 15:00:59 +0530

    updating mistake condition

--- lint_function/check-spelling-code.js
+++ lint_function/check-spelling-code.js
@@ -10,22 +10,34 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 export default async function (input) {
-    dictionary ((err, dict) => {
+    dictionary((err, dict) => {
         if (err) {
             throw err;
         }
-        var spell = nspell(dict)
-        var no_special_characters= input.replace(/[^\w\s]/gi, '')
+        var spell = nspell(dict);
+        var no_special_characters = input.replace(/[^\w\s]/gi, '');
         const words = no_special_characters.split(separatorsRegex);
-        var errors= words
-          .filter((word) => !spell.correct(word))
-          .filter((word) => !word == '')
-          .filter((word) => !includesNumber(word));
+        var errors = words
+            .filter((word) => !spell.correct(word))
+            .filter((word) => !word == '')
+            .filter((word) => !includesNumber(word);
 
-        if ((errors.length > 0) && (mistakes[mistakes.length-1] != errors[errors.length-1])) {
-            mistakes.push(errors);
-            errors = [];
-            console.log("\n There was a spelling mistake: " + mistakes);
+        if (errors.length > 0) {
+            // Concatenate the spelling mistakes into a single string
+            const mistakesString = errors.join(', ');
+            // Print the mistakes and return them in the message
+            console.log("There was a spelling mistake: " + mistakesString);
+            return [{
+                message: `Spelling mistakes found: ${mistakesString}`,
+            }];
+        } else {
+            // Return a message indicating no spelling mistakes found
+            return [{
+                message: "No spelling mistakes found!",
+            }];
         }
-    })
+    });
+    // Sleep and return an empty array if no mistakes were found
+    await sleep(150000);
+    return [];
 };
