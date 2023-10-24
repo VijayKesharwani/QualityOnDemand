0a1a970660392c5d99da8b734dfc296370dae686
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-10-24 16:52:17 +0530

    Update check-telco-specific-terms.js

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -10,7 +10,7 @@ function includesNumber(value) {
 
 export default async function (input) {
   const errors = [];
-  const suggestions = [];
+  const suggestions = new Set();
 
   // Phrases to check for as whole words
   const phrasesToMatch = replacements.map(replacement => replacement.original);
@@ -21,21 +21,23 @@ export default async function (input) {
 
     // Check if the value is a string
     if (typeof value === 'string') {
+      const processedPhrases = new Set(); // To track processed phrases in this property
       for (const phrase of phrasesToMatch) {
         // Use a regular expression to match the phrase as a whole word
         const regex = new RegExp(`\\b${phrase}\\b`, 'g');
 
         // Check if the phrase exists in the value
-        if (regex.test(value)) {
+        if (regex.test(value) && !processedPhrases.has(phrase)) {
           const replacement = replacements.find(replacement => replacement.original === phrase);
           errors.push(replacement);
-          suggestions.push(`Consider replacing '${phrase}' with '${replacement.recommended}'.`);
+          suggestions.add(`Consider replacing '${phrase}' with '${replacement.recommended}'.`);
+          processedPhrases.add(phrase); // Mark the phrase as processed for this property
         }
       }
     }
   }
 
   if (errors.length > 0) {
-    console.log('Hint: Telco-specific terminology found in input: ' + suggestions.join(', '));
+    console.log('Hint: Telco-specific terminology found in input: ' + Array.from(suggestions).join(', '));
   }
 };
