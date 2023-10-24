a0333a1ad7aa3c643ef2830aeca6326ca363a064
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-10-24 16:43:00 +0530

    corrected 2 words match case

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -12,32 +12,30 @@ export default async function (input) {
   const errors = [];
   const suggestions = [];
 
+  // Phrases to check for as whole words
+  const phrasesToMatch = replacements.map(replacement => replacement.original);
+
   // Iterate over properties of the input object
   for (const path in input) {
     const value = input[path];
 
     // Check if the value is a string
     if (typeof value === 'string') {
-      for (const replacement of replacements) {
-        const original = replacement.original;
-        const recommended = replacement.recommended;
+      for (const phrase of phrasesToMatch) {
+        // Use a regular expression to match the phrase as a whole word
+        const regex = new RegExp(`\\b${phrase}\\b`, 'g');
 
-        // Use a regular expression to match 'UE' as a standalone word
-        const regex = new RegExp(`\\b${original}\\b`, 'g');
-
-        // Check if the original word exists in the value
+        // Check if the phrase exists in the value
         if (regex.test(value)) {
+          const replacement = replacements.find(replacement => replacement.original === phrase);
           errors.push(replacement);
-          suggestions.push(`Consider replacing '${original}' with '${recommended}'.`);
+          suggestions.push(`Consider replacing '${phrase}' with '${replacement.recommended}'.`);
         }
       }
     }
   }
 
-  // Check if 'mobile network' is in the suggestions
-  const foundMobileNetwork = suggestions.some((suggestion) => suggestion.includes('mobile network'));
-
-  if (foundMobileNetwork) {
+  if (errors.length > 0) {
     console.log('Hint: Telco-specific terminology found in input: ' + suggestions.join(', '));
   }
 };
