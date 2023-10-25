a5bc6d9296bbf5c65011158f389fa54328b16d07
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-10-25 17:59:50 +0530

    Update check-telco-specific-terms.js

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -22,10 +22,10 @@ export default async function (input) {
         const original = replacement.original;
         const recommended = replacement.recommended;
 
-        // Use a regular expression to match 'UE' as a standalone word
+        // Use a regular expression to match 'original' as a standalone word
         const regex = new RegExp(`\\b${original}\\b`, 'g');
 
-        // Check if the original word exists in the value
+        // Check if 'original' exists in the value
         if (regex.test(value)) {
           errors.push(replacement);
           suggestions.push(`Consider replacing '${original}' with '${recommended}'.`);
@@ -34,10 +34,8 @@ export default async function (input) {
     }
   }
 
-  // Check if 'mobile network' is in the suggestions
-  const foundMobileNetwork = suggestions.some((suggestion) => suggestion.includes('mobile network'));
-
-  if (foundMobileNetwork) {
+  // Check if any word from 'replacements' is in the suggestions
+  if (errors.length > 0) {
     console.log('Hint: Telco-specific terminology found in input: ' + suggestions.join(', '));
   }
 };
