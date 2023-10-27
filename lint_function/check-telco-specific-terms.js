37f2b167b289e5a565ad6fed4963d902560c9423
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-10-27 14:29:47 +0530

    Update check-telco-specific-terms.js

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -1,12 +1,8 @@
-const replacements = [
-  { original: 'UE', recommended: 'device' },
-  { original: 'MSISDN', recommended: 'phone number' },
-  { original: 'mobile network', recommended: 'network' }
-];
-
 export default async function (input) {
   const errors = [];
   const suggestions = [];
+  let mobileNetworkFound = false;
+  let msisdnFound = false;
 
   // Iterate over properties of the input object
   for (const path in input) {
@@ -14,18 +10,16 @@ export default async function (input) {
 
     // Check if the value is a string
     if (typeof value === 'string') {
-      for (const replacement of replacements) {
-        const original = replacement.original;
-        const recommended = replacement.recommended;
+      if (value.includes('mobile network') && !mobileNetworkFound) {
+        errors.push({ original: 'mobile network', recommended: 'network' });
+        suggestions.push(`Consider replacing 'mobile network' with 'network'.`);
+        mobileNetworkFound = true;
+      }
 
-        // Use a regular expression to match 'original' as a standalone word
-        const regex = new RegExp(`\\b${original}\\b`, 'g');
-
-        // Check if 'original' exists in the value
-        if (regex.test(value)) {
-          errors.push(replacement);
-          suggestions.push(`Consider replacing '${original}' with '${recommended}'.`);
-        }
+      if (value.includes('MSISDN') && !msisdnFound) {
+        errors.push({ original: 'MSISDN', recommended: 'phone number' });
+        suggestions.push(`Consider replacing 'MSISDN' with 'phone number'.`);
+        msisdnFound = true;
       }
     }
   }
