a4fa44c0ea12021d452f758c1dcca7720a463dab
VijayKesharwani <122533719+VijayKesharwani@users.noreply.github.com>
2023-11-01 17:02:52 +0530

    Update camara-casing-convention.js

--- lint_function/camara-casing-convention.js
+++ lint_function/camara-casing-convention.js
@@ -3,12 +3,11 @@ export default async function (input) {
   const errors = [];
   const suggestions = [];
   console.log(input);
-  for (const operationId of input) {
+  //for (const operationId of input) {
     if (!isCamelCase(operationId)) {
       errors.push(operationId);
       suggestions.push(`OperationId '${operationId}' should be in camelCase.`);
     }
-  }
 
   if (errors.length > 0) {
     console.log('Hint: OperationId casing convention issues found: ' + suggestions.join(', '));
